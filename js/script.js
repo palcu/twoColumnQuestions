bb85a049163e5f9568a0e9be14a681b2162745ff
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-18 14:33:19 +0300

    Clean script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
-/* Author: Alex Palcuie
-
-	Question Columns
-
-*/
-
-
 $(document).ready( function(){
 
 	function randOrd(){
- 		return (Math.round(Math.random())-0.5); }
+ 		return (Math.round(Math.random()) - 0.5); }
 
 	//Define persons and countries
 	function link(person, country){
@@ -25,9 +18,8 @@ $(document).ready( function(){
 		this.person = person;
 		this.id = id;
 	}
-	
+
 	var links = new Array();
-	
 	links.push(new link("Constantin Brancusi", "Romania"));
 	links.push(new link("Wolfgang Amadeus Mozart", "Austria"));
 	links.push(new link("Gerardus Mercator", "Belgia"));
@@ -49,15 +41,10 @@ $(document).ready( function(){
 	links.push(new link("Mata Hari", "Olanda"));
 	links.push(new link("Papa Ioan Paul al II-lea", "Polonia"));
 	links.push(new link("Jose Saramago", "Portugalia"));
-/*	links.push(new link("Greta Garbo", "Suedia"));
-	links.push(new link("Milan Rastislav Stefanik", "Slovacia"));
-	links.push(new link("Vinko Globokar ", "Slovenia"));
-	links.push(new link("Miguel de Cervantes", "Spania"));
-	links.push(new link("Sandor Petofi", "Ungaria"));*/
 
 	var countries = new Array();
 	var persons = new Array();
-	
+
 	for (var i in links){
 		countries.push(new country(links[i].country, i));
 		persons.push(new person(links[i].person, i));
@@ -76,20 +63,20 @@ $(document).ready( function(){
 		this.name = name;
 		this.score = score;
 	}
-	
+
 	var teams = new Array();
-	
+
 	teams.push(new team("Penguins", 0));
 	teams.push(new team("Geeks", 0));
-	
+
 	for (var i in teams){
 		$('thead tr').append('<td class="' + i + '">' + teams[i].name + '</td>');
 	}
 	for (var i in teams){
 		$('tbody tr').append('<td class="' + i  + '">0</td>');
 	}
-	
-	
+
+
 	//Define events
 	$('.person').bind('click', function() {
 		$('#selected-person').html(links[this.className.split(' ')[0]].person);
@@ -101,28 +88,28 @@ $(document).ready( function(){
 	});
 	$('#check').bind('click', checkAnswer);
 	$('#next').bind('click', nextTurn);
-	
+
 	//Define game
 	var selected_person;
 	var selected_country;
 	var team = 0;
-	
+
 	function nextTurn(){
 		$('thead .' + team).css('font-weight', 'normal');
 		team += 1;
-	
+
 		$('#selected-country').empty();
 		$('#selected-person').empty();
-		
+
 		$('#result').empty();
-		
+
 		var selected_person = 0;
 		var selected_country = 0;
-		
+
 		team %= teams.length;
 		$('thead .' + team).css('font-weight', 'bold');
 	}
-	
+
 	function checkAnswer(){
 		if (selected_person && selected_country){
 			if(selected_person == selected_country){
@@ -136,60 +123,21 @@ $(document).ready( function(){
 			}
 		}
 	}
-	
+
 	function upScore(){
 		teams[team].score++;
 		$('tbody .' + team).html(teams[team].score);
 	}
-	
+
 	//Options
 	$('#goToOptions').bind('click', function(){
 		$('#game').hide('slow');
 		$('#options').show('slow');
 	});
-	
+
 	$('#goToGame').bind('click', function(){
 		$('#options').hide('slow');
 		$('#game').show('slow');
 	});
-	
-	//File saver
-	BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder || window.OperaBlobBuilder;
 
-	// create BlobBuilder
-	var bb = new BlobBuilder;
-	bb.append("Hello World");
-
-	// create Blob / File
-	var blob = bb.getBlob('text/plain');
-
-	// create FileReader
-	var fr = new window.FileReader;
-
-	// when done reading: use dataURL to download file
-	fr.onload = function(e) {
-		document.location = this.result;
-	};
-
-	$('#saveFile').bind('click',function(){
-		var fileSaver = window.saveAs(bb.getBlob(), "test_file");
-		fileSaver.onwriteend = myOnWriteEnd;
-	});
-	
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
